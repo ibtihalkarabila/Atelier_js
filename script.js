3f241ce84a83e35258f2e62a1bfdf35cfc1938ef
ibtihalkarabila <ibtihal.karabila.estn25@ump.ac.ma>
2025-12-27 15:52:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
 
-const notes = [];
-
-for (let i = 0; i < 5; i++) {
-    const note = parseFloat(prompt("Entrez la note " + (i + 1) + " :"));
-    if (!isNaN(note)) {
-        notes.push(note);
+function calculerReduction() {
+    let montant = prompt("Entrez le montant de vos achats en Dh :");
+    if (montant === null || montant.trim() === "") {
+        console.log("Aucun montant saisi.");
+        return;
     }
+    montant = Number(montant);
+
+    if (isNaN(montant) || montant < 0) {
+        console.log("Veuillez entrer un montant valide et positif !");
+        return;
+    }
+    let reduction = 0
+    if (montant < 200) {
+        reduction = montant * 0.05;
+    } else if (montant <= 500) {
+        reduction = montant * 0.10;
+    } else {
+        reduction = montant * 0.20;
+    }
+    let total = montant - reduction;
+    console.log(`Pour un montant de montant}D${montant}h, votre réduction est de ${reduction}Dh et le total est ${total}Dh.`);
 }
-
-console.log("Tableau des notes :", notes);
-
-function calculerMoyenne(notesArray) {
-    let somme = 0;
-    let nbNotesValides = 0;
-    
-    for (let note of notesArray) {
-        if (note < 0) {
-            console.log("Note negative (" + note + ") ignoree.");
-        } else {
-            somme += note;
-            nbNotesValides++;
-        }
-    }
-    
-    return nbNotesValides > 0 ? somme / nbNotesValides : 0;
-}
-
-const calculerMoyenneFlechee = (notesArray) => {
-    let somme = 0;
-    let nbNotesValides = 0;
-    
-    for (let note of notesArray) {
-        if (note < 0) {
-            console.log("Note negative (" + note + ") ignoree.");
-        } else {
-            somme += note;
-            nbNotesValides++;
-        }
-    }
-    
-    return nbNotesValides > 0 ? somme / nbNotesValides : 0;
-};
-
-const moyenne = calculerMoyenne(notes);
-const moyenneFlechee = calculerMoyenneFlechee(notes);
-
-console.log("Moyenne (fonction classique) : " + moyenne);
-console.log("Moyenne (fonction flechee) : " + moyenneFlechee);
-
-document.body.innerHTML = 
-    '<h1>Resultat du calcul des moyennes</h1>' +
-    '<p>Notes entrees : ' + notes.join(', ') + '</p>' +
-    '<p>Moyenne (fonction classique) : ' + moyenne.toFixed(2) + '</p>' +
-    '<p>Moyenne (fonction flechee) : ' + moyenneFlechee.toFixed(2) + '</p>';
+calculerReduction();
